152627b0bd867220dc44128687015e817467d301
Keith <755170776@qq.com>
2019-01-15 16:32:42 +0800

    import QLearning first example
    movan python QLearning的示例程序

---
+++ Reinforcement Learning/QLearning/test1/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+# @Time             : 2019-01-14 21:45
+# @Author           : Keith
+# @File             : __init__.py.py
+# @Software         : PyCharm
+# @About            : 
